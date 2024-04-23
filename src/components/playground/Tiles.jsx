b7c07ec9bc18f540c9b189f3de008c16b5deff45
Thendup Tsering <thendupcodes@gmail.com>
2024-04-23 09:05:34 -0400

    Update colors

--- src/components/playground/Tiles.jsx
+++ src/components/playground/Tiles.jsx
@@ -1,31 +1,10 @@
 import anime from "animejs";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useResizer } from "../../hooks/useResizer";
 
-const colors = [
-  '#001219',
-  '#005F73',
-  '#0A9396',
-  '#94D2BD',
-  '#E9D8A6',
-  '#EE9B00',
-  '#CA6702',
-  '#BB3E03',
-  '#AE2012',
-  '#9B2226',
-  '#F94144',
-  '#F3722C',
-  '#F8961E',
-  '#F9844A',
-  '#F9C74F',
-  '#90BE6D',
-  '#43AA8B',
-  '#4D908E',
-  '#577590',
-  '#277DA1',
-];
-
 export default function Tiles () {
+  const [toggled, setToggled] = useState(false);
+
   const { windowWidth, windowHeight } = useResizer();
 
   const [cols, rows] = useMemo(() => {
@@ -37,9 +16,11 @@ export default function Tiles () {
   }, [cols, rows])
 
   const handleOnClick = (idx) => {
+    setToggled(!toggled);
+
     anime({
       targets: ".Tile",
-      backgroundColor: colors[Math.floor(Math.random() * colors.length)],
+      opacity: toggled ? 1 : 0,
       delay: anime.stagger(50, {
         grid: [cols, rows],
         from: idx,
