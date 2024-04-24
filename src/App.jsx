6d140b9f2531f4e4a48dafc3debca55bcbb1965a
Thendup Tsering <thendupcodes@gmail.com>
2024-04-24 14:11:14 -0400

    Add route and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom';
+
+import Home from './components/Home';
 
 function App() {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
 
   return (
     <div className="App">
-      <div className="App__navbar">
-        <nav className="Navbar">
-          <Link className="Navbar__link" to="/">Home</Link>
+      {isHome ? (
+        <>
+          <div className="App__navbar">
+            <nav className="Navbar">
+              <Link className="Navbar__link" to="/quote-tiles">Quotes & Tiles</Link>
+            </nav>
+          </div>
 
-          <Link className="Navbar__link" to="/tiles">Tiles</Link>
-        </nav>
-      </div>
-
-      <div className="App__body">
-        <Outlet />
-      </div>
-
+          <div className="App__body">
+            <Home />
+          </div>
+        </>
+      ) : (
+        <div className="App__body">
+          <Outlet />
+        </div>
+      )}
     </div>
   )
 }
