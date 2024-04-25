eb46f627f7f08ec8024c8ce321bb020f9e9f3e47
Thendup Tsering <thendupcodes@gmail.com>
2024-04-25 09:45:10 -0400

    Add custom tooltip component

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Tooltip ({ content, children, direction, delay = 200 }) {
+  const [show, setShow] = useState();
+
+  const style = {
+    transition: `opacity ${delay}ms, visibility ${delay}ms`,
+    transitionDelay: `${delay}ms`,
+  };
+
+  return (
+     <div
+      tabIndex="0"
+      className="Tooltip-Container"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      <div
+        style={style}
+        className={`Tooltip__tooltip ${!show ? 'Tooltip__tooltip--hide' : ''} Tooltip__tooltip--${direction}`}
+      >
+        {content}
+      </div>
+      
+      <div className="Tooltip__primary">{children}</div>
+    </div>
+  );
+}
