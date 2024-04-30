b7d40a612983dbd66346d9a54ca1133bf233bc4f
Thendup Tsering <thendupcodes@gmail.com>
2024-04-30 09:37:15 -0400

    Add logo using icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 
 import Home from './components/Home';
@@ -6,55 +6,79 @@ import Home from './components/Home';
 function App() {
   const location = useLocation();
 
+  const [loadingPage, setLoadingPage] = useState(true);
   const [showMenu, setShowMenu] = useState(false);
-
+  
   const isHome = location.pathname === '/';
 
   const toggleMenu = () => {
     setShowMenu(!showMenu)
   }
 
+  const loadPage = () => {
+    setTimeout(() => {
+      setLoadingPage(false);
+    }, 2000)
+  }
+
+  useEffect(() => {
+    loadPage();
+  }, [])
+
   return (
     <div className="App">
       {isHome ? (
         <>
-          <div className="App__navbar">
-            <nav id="desktop-nav" className="Navbar">
-              <div className="Navbar__logo">Thendup Tsering</div>
+          {loadingPage ? (
+            <>
+            LOADER
+            </>
+          ) : (
+            <>
+              <div className="App__navbar">
+                <nav id="desktop-nav" className="Navbar">
+                  <div className="Navbar__logo">
+                    <div className="Logo fa-stack fa-2x">
+                      <i className="Logo__left fa-solid fa-angle-left fa-stack-2x" />
+                      <i className="Logo__right fa-solid fa-angle-right fa-stack-2x" />
+                    </div>
+                  </div>
 
-              <div className="Navbar__links">
-                <Link className="Navbar__link">About me</Link>
-                <Link className="Navbar__link">Experience</Link>
-                <Link className="Navbar__link">Projects</Link>
-                <Link className="Navbar__link">Contact</Link>
+                  <div className="Navbar__links">
+                    <Link className="Navbar__link">About me</Link>
+                    <Link className="Navbar__link">Experience</Link>
+                    <Link className="Navbar__link">Projects</Link>
+                    <Link className="Navbar__link">Contact</Link>
+                  </div>
+                </nav>
+
+                <nav id="hamburger-nav" className="Hamburger-Menu">
+                  <div className="Hamburger-Menu__logo">Thendup Tsering</div>
+
+                  <button className="Hamburger-Menu__button Button Button--icon" onClick={toggleMenu}>
+                    {showMenu ? (
+                      <i className="fa-solid fa-x" />
+                    ) : (
+                      <i className="fa-solid fa-bars" />
+                    )}
+                  </button>
+
+                  {showMenu && (
+                    <div className="Hamburger-Menu__links">
+                      <Link className="Hamburger-Menu__link">About me</Link>
+                      <Link className="Hamburger-Menu__link">Experience</Link>
+                      <Link className="Hamburger-Menu__link">Projects</Link>
+                      <Link className="Hamburger-Menu__link">Contact</Link>
+                    </div>
+                  )}
+                </nav>
               </div>
-            </nav>
 
-            <nav id="hamburger-nav" className="Hamburger-Menu">
-              <div className="Hamburger-Menu__logo">Thendup Tsering</div>
-
-              <button className="Hamburger-Menu__button Button Button--icon" onClick={toggleMenu}>
-                {showMenu ? (
-                  <i className="fa-solid fa-x" />
-                ) : (
-                  <i className="fa-solid fa-bars" />
-                )}
-              </button>
-
-              {showMenu && (
-                <div className="Hamburger-Menu__links">
-                  <Link className="Hamburger-Menu__link">About me</Link>
-                  <Link className="Hamburger-Menu__link">Experience</Link>
-                  <Link className="Hamburger-Menu__link">Projects</Link>
-                  <Link className="Hamburger-Menu__link">Contact</Link>
-                </div>
-              )}
-            </nav>
-          </div>
-
-          <div className="App__body">
-            <Home />
-          </div>
+              <div className="App__body">
+                <Home />
+              </div>
+            </>
+          )}
         </>
       ) : (
         <div className="App__body">
