f34753d345f0a5c9a0286695bcbd500bc2befdbb
Thendup Tsering <thendupcodes@gmail.com>
2024-04-29 20:28:52 -0400

    Add other sections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import Tooltip from './Tooltip';
 
+import { millisecondsYear } from '../constants/constants';
+
 const RESUME_URL = '/_thenduptsering_resume.pdf';
+const DATE_STARTED_WORKING = '06/01/2015';
 
 export default function Home () {
 
@@ -13,19 +16,20 @@ export default function Home () {
     document.body.removeChild(anchor)
   }
 
+  const now = new Date();
+  const experienceStart = new Date(DATE_STARTED_WORKING);
+
+  const yearsSince = Math.floor((now.getTime() - experienceStart.getTime()) / millisecondsYear);
+
   return (
     <div className="Home">
       <section id="hero" className="Home__section Home__section--hero">
-        <div className="Home__section-pic-container">
-          <img className="Home__section-pic" src="/headshot.jpg" alt="Thendup headshot" />
-        </div>
-
         <div className="Home__section-info-right">
           <p className="Home__section-text-2">Hello, I am</p>
 
           <h1 className="Home__section-text-main">Thendup Tsering</h1>
 
-          <p className="Home__section-text-1">Senior Front-End Software&nbsp;Engineer</p>
+          <p className="Home__section-text-1">Senior Front-End Software Engineer</p>
 
           <div className="Home__section-buttons">
             <button className="Home__section-button Button Button--primary" onClick={downloadResume}>My resume</button>
@@ -55,7 +59,7 @@ export default function Home () {
                 </h3>
 
                 <p className="Home__section-info-box-details">
-                  8+ years<br />Front-end software development
+                  {yearsSince}+ years<br />Front-end development
                 </p>
               </div>
 
@@ -79,6 +83,126 @@ export default function Home () {
             <div className="Home__section-info-text">
               Hello! I am Thendup Tsering....
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="skills" className="Home__section Home__section--skills">
+        <h1 className="Home__section-heading">Skills</h1>
+        
+        <div className="Home__section-info">
+          <div className="Home__section-info-right">
+            <div className="Home__section-info-box-container">
+              <div className="Home__section-info-box">
+                <div className="Home__section-info-box-icon">
+                  <i className="fa-solid fa-medal" />
+                </div>
+
+                <h3 className="Home__section-info-box-title">
+                  Experience
+                </h3>
+
+                <p className="Home__section-info-box-details">
+                  {yearsSince}+ years<br />Front-end development
+                </p>
+              </div>
+
+              <div className="Home__section-info-box">
+                <div className="Home__section-info-box-icon">
+                  <i className="fa-solid fa-graduation-cap" />
+                </div>
+
+                <h3 className="Home__section-info-box-title">
+                  Education
+                </h3>
+
+                <p className="Home__section-info-box-details">
+                  Bachelors of Computer Science
+                  <br />
+                  University of Waterloo 2017
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="projects" className="Home__section Home__section--projects">
+        <h1 className="Home__section-heading">Projects</h1>
+        
+        <div className="Home__section-info">
+          <div className="Home__section-info-right">
+            <div className="Home__section-info-box-container">
+              <div className="Home__section-info-box">
+                <div className="Home__section-info-box-icon">
+                  <i className="fa-solid fa-medal" />
+                </div>
+
+                <h3 className="Home__section-info-box-title">
+                  Experience
+                </h3>
+
+                <p className="Home__section-info-box-details">
+                  {yearsSince}+ years<br />Front-end development
+                </p>
+              </div>
+
+              <div className="Home__section-info-box">
+                <div className="Home__section-info-box-icon">
+                  <i className="fa-solid fa-graduation-cap" />
+                </div>
+
+                <h3 className="Home__section-info-box-title">
+                  Education
+                </h3>
+
+                <p className="Home__section-info-box-details">
+                  Bachelors of Computer Science
+                  <br />
+                  University of Waterloo 2017
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="contact" className="Home__section Home__section--contact">
+        <h1 className="Home__section-heading">Contact me</h1>
+        
+        <div className="Home__section-info">
+          <div className="Home__section-info-right">
+            <div className="Home__section-info-box-container">
+              <div className="Home__section-info-box">
+                <div className="Home__section-info-box-icon">
+                  <i className="fa-solid fa-medal" />
+                </div>
+
+                <h3 className="Home__section-info-box-title">
+                  Experience
+                </h3>
+
+                <p className="Home__section-info-box-details">
+                  {yearsSince}+ years<br />Front-end development
+                </p>
+              </div>
+
+              <div className="Home__section-info-box">
+                <div className="Home__section-info-box-icon">
+                  <i className="fa-solid fa-graduation-cap" />
+                </div>
+
+                <h3 className="Home__section-info-box-title">
+                  Education
+                </h3>
+
+                <p className="Home__section-info-box-details">
+                  Bachelors of Computer Science
+                  <br />
+                  University of Waterloo 2017
+                </p>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const millisecondsDay = 1000 * 60 * 60 * 24;
+export const millisecondsYear = millisecondsDay * 365;
+
