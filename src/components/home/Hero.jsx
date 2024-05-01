bd1439ccb7800f3ed7d358a88ec7dda3fe1284e2
Thendup Tsering <thendupcodes@gmail.com>
2024-05-01 12:42:08 -0400

    Move downloadResume to helper file

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,13 +1,4 @@
-const RESUME_URL = '/_thenduptsering_resume.pdf';
-
-const downloadResume = () => {
-  const anchor = document.createElement('a')
-  anchor.href = RESUME_URL
-  anchor.download = RESUME_URL.split('/').pop()
-  document.body.appendChild(anchor)
-  anchor.click()
-  document.body.removeChild(anchor)
-}
+import downloadResume from '../../helpers/downloadResume';
 
 export default function Hero () {
   return (
---
+++ src/helpers/downloadResume.js
@@ -0,0 +1,12 @@
+const RESUME_URL = '/_thenduptsering_resume.pdf';
+
+const downloadResume = () => {
+  const anchor = document.createElement('a')
+  anchor.href = RESUME_URL
+  anchor.download = RESUME_URL.split('/').pop()
+  document.body.appendChild(anchor)
+  anchor.click()
+  document.body.removeChild(anchor)
+}
+
+export default downloadResume;
