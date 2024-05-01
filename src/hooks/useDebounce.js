eac03f90349e230634dfcf33e6893e2f82a11f4e
Thendup Tsering <thendupcodes@gmail.com>
2024-05-01 16:04:21 -0400

    Add useful hooks

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,20 @@
+import { useRef } from 'react';
+
+const useDebounce = (cb, delay = 1000) => {
+  // create a timeout ref so that it persists through re-renders
+  let timeout = useRef(null);
+
+  return (...args) => {
+    // if the function is called again, clear the previous timeout immediately
+    if (timeout.current != null) {
+      clearTimeout(timeout.current);
+    }
+
+    // run the callback after a set period of time
+    timeout.current = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+}
+
+export default useDebounce;
