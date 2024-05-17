79cc3949703edbb7bcfc519356ea76da6898b4cd
Thendup Tsering <thendupcodes@gmail.com>
2024-05-17 09:43:17 -0400

    Formatting ./src/components/home/Navbar.jsx

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -4,70 +4,82 @@ import Logo from '@/components/Logo';
 
 import downloadResume from '@/helpers/downloadResume';
 
-export default function Navbar ({
-  loadingNavBar,
-  loadedNavBar,
-  loadPage,
-}) {
-  return (
-    <nav id="desktop-nav" className={`Navbar ${loadingNavBar ? 'Navbar--loading' : ''} ${loadedNavBar ? 'Navbar--loaded' : ''}`}>
-      <div role="button" onClick={loadPage} className="Navbar__logo" style={{animationDelay: '0ms'}}>
-        <Logo />
-      </div>
+export default function Navbar({ loadingNavBar, loadedNavBar, loadPage }) {
+	return (
+		<nav
+			id="desktop-nav"
+			className={`Navbar ${loadingNavBar ? 'Navbar--loading' : ''} ${
+				loadedNavBar ? 'Navbar--loaded' : ''
+			}`}
+		>
+			<div
+				role="button"
+				onClick={loadPage}
+				className="Navbar__logo"
+				style={{ animationDelay: '0ms' }}
+			>
+				<Logo />
+			</div>
 
-      <div className="Navbar__links">
-        <ScrollLink
-          className="Navbar__link"
-          to="about"
-          style={{ animationDelay: '200ms' }}
-          smooth={true}
-          duration={500}
-        >
-          About me
-        </ScrollLink>
+			<div className="Navbar__links">
+				<ScrollLink
+					className="Navbar__link"
+					to="about"
+					style={{ animationDelay: '200ms' }}
+					smooth={true}
+					duration={500}
+				>
+					About me
+				</ScrollLink>
 
-        <ScrollLink
-          className="Navbar__link"
-          to="experience"
-          style={{ animationDelay: '400ms' }}
-          smooth={true}
-          duration={500}
-        >
-          Experience
-        </ScrollLink>
+				<ScrollLink
+					className="Navbar__link"
+					to="experience"
+					style={{ animationDelay: '400ms' }}
+					smooth={true}
+					duration={500}
+				>
+					Experience
+				</ScrollLink>
 
-        <ScrollLink
-          className="Navbar__link"
-          to="skills"
-          style={{ animationDelay: '600ms' }}
-          smooth={true}
-          duration={500}
-        >
-          Skills
-        </ScrollLink>
+				<ScrollLink
+					className="Navbar__link"
+					to="skills"
+					style={{ animationDelay: '600ms' }}
+					smooth={true}
+					duration={500}
+				>
+					Skills
+				</ScrollLink>
 
-        <ScrollLink
-          className="Navbar__link"
-          to="projects"
-          style={{ animationDelay: '800ms' }}
-          smooth={true}
-          duration={500}
-        >
-          Projects
-        </ScrollLink>
+				<ScrollLink
+					className="Navbar__link"
+					to="projects"
+					style={{ animationDelay: '800ms' }}
+					smooth={true}
+					duration={500}
+				>
+					Projects
+				</ScrollLink>
 
-        <ScrollLink
-          className="Navbar__link"
-          to="contact"
-          style={{ animationDelay: '1000ms' }}
-          smooth={true}
-          duration={500}
-        >
-          Contact
-        </ScrollLink>
+				<ScrollLink
+					className="Navbar__link"
+					to="contact"
+					style={{ animationDelay: '1000ms' }}
+					smooth={true}
+					duration={500}
+				>
+					Contact
+				</ScrollLink>
 
-        <button className="Navbar__button Button Button--default" onClick={downloadResume} style={{animationDelay: '1200ms'}}>Resume</button>
-      </div>
-    </nav>
-  );
+				<button
+					className="Navbar__button Button Button--default"
+					onClick={downloadResume}
+					style={{ animationDelay: '1200ms' }}
+				>
+					Resume
+				</button>
+			</div>
+		</nav>
+	);
 }
