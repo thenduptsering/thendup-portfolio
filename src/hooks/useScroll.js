393c79b9ae37cd2b85203cd3fcfdbb378e07e799
Thendup Tsering <thendupcodes@gmail.com>
2024-05-17 09:43:17 -0400

    Formatting ./src/hooks/useScroll.js

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,30 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const useScroll = () => {
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-  const [bodyOffset, setBodyOffset] = useState(document.body.getBoundingClientRect());
-  const [scrollY, setScrollY] = useState(bodyOffset.top);
-  const [scrollX, setScrollX] = useState(bodyOffset.left);
-  const [scrollDir, setScrollDir] = useState();
+	const [lastScrollTop, setLastScrollTop] = useState(0);
+	const [bodyOffset, setBodyOffset] = useState(
+		document.body.getBoundingClientRect()
+	);
+	const [scrollY, setScrollY] = useState(bodyOffset.top);
+	const [scrollX, setScrollX] = useState(bodyOffset.left);
+	const [scrollDir, setScrollDir] = useState();
 
-  const handleScroll = () => {
-    const clientRect = document.body.getBoundingClientRect();
-    setBodyOffset(clientRect);
-    setScrollY(-clientRect.top);
-    setScrollX(clientRect.left);
-    setScrollDir(lastScrollTop > -clientRect.top ? "up" : "down");
-    setLastScrollTop(-clientRect.top);
-  };
+	const handleScroll = () => {
+		const clientRect = document.body.getBoundingClientRect();
+		setBodyOffset(clientRect);
+		setScrollY(-clientRect.top);
+		setScrollX(clientRect.left);
+		setScrollDir(lastScrollTop > -clientRect.top ? 'up' : 'down');
+		setLastScrollTop(-clientRect.top);
+	};
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  });
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	});
 
-  return { scrollY, scrollX, scrollDir };
-}
+	return { scrollY, scrollX, scrollDir };
+};
 
 export default useScroll;
