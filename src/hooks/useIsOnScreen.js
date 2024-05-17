e8de025dee2a01d6a5c6fd1a5e47dea1c824a885
Thendup Tsering <thendupcodes@gmail.com>
2024-05-17 09:43:17 -0400

    Formatting ./src/hooks/useIsOnScreen.js

--- src/hooks/useIsOnScreen.js
+++ src/hooks/useIsOnScreen.js
@@ -1,27 +1,32 @@
 import { useEffect, useMemo, useState } from 'react';
 
 export default function useIsOnScreen({ ref, threshold = 0.5 }) {
-  if (ref == null) return false;
+	if (ref == null) return false;
 
-  const [isOnScreen, setIsOnScreen] = useState(false)
+	const [isOnScreen, setIsOnScreen] = useState(false);
 
-  const observer = useMemo(() => new IntersectionObserver(
-    ([entry]) => {
-      setIsOnScreen(entry.isIntersecting)
-    }, {
-      root: null, // Use viewport as the root
-      rootMargin: '0px', // No margin
-      threshold, // % visibility threshold
-    }
-  ), [ref])
+	const observer = useMemo(
+		() =>
+			new IntersectionObserver(
+				([entry]) => {
+					setIsOnScreen(entry.isIntersecting);
+				},
+				{
+					root: null, // Use viewport as the root
+					rootMargin: '0px', // No margin
+					threshold, // % visibility threshold
+				}
+			),
+		[ref]
+	);
 
-  useEffect(() => {
-    observer.observe(ref.current);
+	useEffect(() => {
+		observer.observe(ref.current);
 
-    return () => {
-      observer.disconnect();
-    }
-  }, [])
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
 
-  return isOnScreen;
+	return isOnScreen;
 }
